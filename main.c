e5bdeb6231a83b62a60c3e2e6e3121c8982a6561
Iain MacDonald <ijmacd@gmail.com>
2021-06-19 23:28:08 +0800

    Support specifiying SQL file in command line

--- main.c
+++ main.c
@@ -1,26 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "query.h"
 
 void printUsage (const char* name) {
-    printf("Usage:\n\t%s \"SELECT <fields, ...> FROM <file> [WHERE <field> = <value>]\"\n", name);
+    printf("Usage:\n\t%1$s \"SELECT <fields, ...> FROM <file> [WHERE <field> = <value>]\"\n\t%1$s -- file.sql\n", name);
 }
 
 int main (int argc, char * argv[]) {
+    char buffer[1024];
+
+    if (argc > 2) {
+        if (strcmp(argv[1], "--") == 0) {
+            FILE *f = fopen(argv[2], "r");
+
+            if (!f) {
+                fprintf(stderr, "Couldn't open file %s\n", argv[2]);
+                return -1;
+            }
+
+            size_t count = fread(buffer, 1, 1024, f);
+
+            if (count > 0) {
+                query(buffer);
+                return 0;
+            }
+
+            printf("File '%s' was empty\n", argv[2]);
+            return -1;
+        }
+
+        printf("I got '%s'\n", argv[1]);
+
+        printUsage(argv[0]);
+        return -1;
+    }
+
     if (argc > 1) {
         query(argv[1]);
         return 0;
     }
 
-    char buffer[1024];
-    size_t count = fread(buffer, 1, 1024, stdin);
-    if (count > 0) {
-        buffer[count] = '\0';
-        query(buffer);
-        return 0;
+    if ((fseek(stdin, 0, SEEK_END), ftell(stdin)) > 0) {
+        rewind(stdin);
+
+        size_t count = fread(buffer, 1, 1024, stdin);
+        if (count > 0) {
+            buffer[count] = '\0';
+            query(buffer);
+            return 0;
+        }
     }
     
     printUsage(argv[0]);
-    exit(-1);
+    return -1;
 }
