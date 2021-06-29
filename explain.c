766381d7c1a0defc183356eb356c4f1d131746a5
Iain MacDonald <ijmacd@gmail.com>
2021-06-29 18:12:48 +0800

    Update explain to match new execution plan

--- explain.c
+++ explain.c
@@ -42,49 +42,86 @@ int explain_select_query (
         return 0;
     }
 
-    if ((flags & FLAG_PRIMARY_KEY_SEARCH) && predicate_op == OPERATOR_EQ) {
-        int cost = log_rows;
-        printf("%d\tTABLE ACCESS BY ROWID\t%s\t%d\t%d\n", op++, table, 1, cost);
-        printf("%d\tINDEX UNIQUE SCAN\t%s\t%d\t%d\n", op++, predicate_field, 1, cost);
+    if (flags & FLAG_PRIMARY_KEY_SEARCH) {
+        if (predicate_op == OPERATOR_EQ) {
+            int cost = log_rows;
+            printf("%d\tTABLE ACCESS BY ROWID\t%s\t%d\t%d\n", op++, table, 1, cost);
+            printf("%d\tINDEX UNIQUE SCAN\t%s\t%d\t%d\n", op++, predicate_field, 1, cost);
+            return 0;
+        }
+
+        row_estimate /= 2;
+
+        if (flags & FLAG_ORDER) {
+            printf("%d\tSORT ORDER BY\t\t\t%d\t%ld\n", op++, row_estimate, (long)row_estimate * row_estimate);
+        }
+
+        printf("%d\tTABLE ACCESS BY ROWID\t%s\t%d\t%d\n", op++, table, row_estimate, row_estimate);
+        printf("%d\tINDEX RANGE SCAN\t%s\t%d\t%d\n", op++, predicate_field, row_estimate, row_estimate);
         return 0;
     }
 
-    if ((flags & FLAG_HAVE_PREDICATE) && predicate_op == OPERATOR_EQ) {
+    if (flags & FLAG_HAVE_PREDICATE) {
         char index_filename[TABLE_MAX_LENGTH + 10];
         sprintf(index_filename, "%s.unique.csv", predicate_field);
 
         struct DB index_db;
 
         if (openDB(&index_db, index_filename) == 0) {
-            int cost = log_rows;
-            printf("%d\tTABLE ACCESS BY ROWID\t%s\t%d\t%d\n", op++, table, 1, cost);
-            printf("%d\tINDEX UNIQUE SCAN\t%s\t%d\t%d\n", op++, predicate_field, 1, cost);
+            if (predicate_op == OPERATOR_EQ) {
+                int cost = log_rows;
+                printf("%d\tTABLE ACCESS BY ROWID\t%s\t%d\t%d\n", op++, table, 1, cost);
+                printf("%d\tINDEX UNIQUE SCAN\t%s\t%d\t%d\n", op++, predicate_field, 1, cost);
 
+                return 0;
+            }
+
+            row_estimate /= 2;
+
+            if (flags & FLAG_ORDER) {
+                printf("%d\tSORT ORDER BY\t\t\t%d\t%ld\n", op++, row_estimate, (long)row_estimate * row_estimate);
+            }
+
+            printf("%d\tTABLE ACCESS BY ROWID\t%s\t%d\t%d\n", op++, table, row_estimate, row_estimate);
+            printf("%d\tINDEX RANGE SCAN\t%s\t%d\t%d\n", op++, predicate_field, row_estimate, row_estimate);
             return 0;
         }
     }
 
     if (flags & FLAG_HAVE_PREDICATE) {
-        // 10,000 is a wild guess at index statistics
-        row_estimate = row_estimate / 10000;
 
-        if (row_estimate < 1) {
-            row_estimate = 1;
+        if (flags & FLAG_HAVE_PREDICATE) {
+            if (predicate_op == OPERATOR_EQ) {
+                // 10,000 is a wild guess at index statistics
+                row_estimate = row_estimate / 10000;
+            } else if (predicate_op == OPERATOR_NE) {
+                // 10,000 is a wild guess at index statistics
+                row_estimate = row_estimate - (row_estimate / 10000);
+            } else {
+                row_estimate = row_estimate / 2;
+            }
+
+            if (row_estimate < 1) {
+                row_estimate = 1;
+            }
         }
-    }
 
-    if (flags & FLAG_ORDER) {
-        printf("%d\tSORT ORDER BY\t\t\t%d\t%ld\n", op++, row_estimate, (long)row_estimate * row_estimate);
-    }
+        if (flags & FLAG_ORDER) {
+            printf("%d\tSORT ORDER BY\t\t\t%d\t%ld\n", op++, row_estimate, (long)row_estimate * row_estimate);
+        }
 
-    if ((flags & FLAG_HAVE_PREDICATE) && predicate_op == OPERATOR_EQ) {
         char index_filename[TABLE_MAX_LENGTH + 10];
         sprintf(index_filename, "%s.index.csv", predicate_field);
 
         struct DB index_db;
 
         if (openDB(&index_db, index_filename) == 0) {
-            int cost = log_rows * 2;
+            int cost = row_estimate;
+
+            if (predicate_op == OPERATOR_EQ) {
+                cost = log_rows * 2;
+            }
+
             printf("%d\tTABLE ACCESS BY ROWID\t%s\t%d\t%d\n", op++, table, row_estimate, cost);
             printf("%d\tINDEX RANGE SCAN\t%s\t%d\t%d\n", op++, predicate_field, row_estimate, cost);
 
