452939c6902c4d9ecb7cb6d128f8200266fb4b30
Iain MacDonald <IJMacD@gmail.com>
2022-01-05 15:44:19 +0800

    Experiment with double pointer version of tokenizer

--- src/token.c
+++ src/token.c
@@ -3,57 +3,116 @@
 
 #include "token.h"
 
-void skipWhitespace (const char *string, size_t *index) {
-    while (string[*index] != '\0') {
-        while(isspace(string[*index])) { (*index)++; }
+void skipWhitespace2 (const char **string) {
+    while (**string != '\0') {
+        while(isspace(**string)) { (*string)++; }
 
-        if (strncmp(string + *index, "--", 2) == 0) {
-            skipLine(string, index);
+        if (strncmp(*string, "--", 2) == 0) {
+            skipLine2(string);
         } else {
             break;
         }
     }
 }
+void skipWhitespace (const char *string, size_t *index) {
+    const char *ptr = string + *index;
+    const char *start = ptr;
+    skipWhitespace2(&ptr);
 
-void skipToken (const char *string, size_t *index) {
-    if (string[*index] == '\'') {
+    *index += ptr - start;
+}
+
+void skipToken2 (const char **string) {
+    if (**string == '\'') {
         // Skip open quote
-        (*index)++;
+        (*string)++;
 
-        while (string[*index] != '\0' && string[*index] != '\'') {
-            (*index)++;
+        while (**string != '\0' && **string != '\'') {
+            (*string)++;
         }
 
         // Skip close quote
-        if (string[*index] != '\0') {
-            (*index)++;
+        if (**string != '\0') {
+            (*string)++;
         }
     }
-    else if (string[*index] == '"') {
+    else if (**string == '"') {
         // Skip open quote
-        (*index)++;
+        (*string)++;
 
-        while (string[*index] != '\0' && string[*index] != '"') {
-            (*index)++;
+        while (**string != '\0' && **string != '"') {
+            (*string)++;
         }
 
         // Skip close quote
-        if (string[*index] != '\0') {
-            (*index)++;
+        if (**string != '\0') {
+            (*string)++;
         }
     }
     else {
-        while (!iscntrl(string[*index]) && string[*index] != ' ' && string[*index] != ',') {
-            (*index)++;
+        while (!iscntrl(**string) && **string != ' ' && **string != ',') {
+            (*string)++;
         }
     }
 }
 
-void skipLine (const char *string, size_t *index) {
-    while (string[*index] != '\n' && string[*index] != '\0') {
-        (*index)++;
+void skipToken (const char *string, size_t *index) {
+    const char *ptr = string + *index;
+    const char *start = ptr;
+    skipToken2(&ptr);
+
+    *index += ptr - start;
+}
+
+void skipLine2 (const char **string) {
+    while (**string != '\n' && **string != '\0') {
+        (*string)++;
     }
-    if (string[*index] == '\n') (*index)++;
+    if (**string == '\n') (*string)++;
+}
+
+void skipLine (const char *string, size_t *index) {
+    const char *ptr = string + *index;
+    const char *start = ptr;
+    skipLine2(&ptr);
+
+    *index += ptr - start;
+}
+
+/**
+ * @brief Get the next token in the stream
+ *
+ * @param string Pointer to input string
+ * @param token Output string
+ * @param token_max_length Storage size of output string
+ * @return int Length of token
+ */
+int getToken2 (const char **string, char *token, int token_max_length) {
+
+    skipWhitespace2(string);
+
+    if (**string == '\0') {
+        return -1;
+    }
+    const char *start = *string;
+
+    // printf("Token starts at %d\n", start_index);
+
+    skipToken2(string);
+
+    int token_length = *string - start;
+
+    if (token_length > token_max_length) {
+        return -1;
+    }
+
+    // printf("Token is %d characters\n", token_length);
+
+    memcpy(token, *string, token_length);
+
+    token[token_length] = '\0';
+
+    return token_length;
 }
 
 /**
@@ -66,18 +125,48 @@ void skipLine (const char *string, size_t *index) {
  * @return int Length of token
  */
 int getToken (const char *string, size_t *index, char *token, int token_max_length) {
-    skipWhitespace(string, index);
+    const char *ptr = string + *index;
+    int result = getToken2(&ptr, token, token_max_length);
+    if (result >= 0) {
+        *index += result;
+    }
+    return result;
+}
 
-    if (string[*index] == '\0') {
+/**
+ * @brief Get the next token in the stream, optionally quoted
+ *
+ * @param string
+ * @param index
+ * @param token
+ * @param token_max_length
+ * @return int 0 means no quotes, ~~1 means single quotes~~, 2 means double quotes
+ */
+int getQuotedToken2 (const char **string, char *token, int token_max_length) {
+
+    skipWhitespace2(string);
+
+    if (**string == '\0') {
         return -1;
     }
-    int start_index = *index;
+
+    int quoted_flag = (**string == '"' ? 2 : 0);
+
+    const char * start = *string;
+
+    if (quoted_flag) {
+        start++;
+    }
 
     // printf("Token starts at %d\n", start_index);
 
-    skipToken(string, index);
+    skipToken2(string);
 
-    int token_length = *index - start_index;
+    int token_length = *string - start;
+
+    if (quoted_flag) {
+        token_length--;
+    }
 
     if (token_length > token_max_length) {
         return -1;
@@ -85,11 +174,11 @@ int getToken (const char *string, size_t *index, char *token, int token_max_leng
 
     // printf("Token is %d characters\n", token_length);
 
-    memcpy(token, string + start_index, token_length);
+    memcpy(token, start, token_length);
 
     token[token_length] = '\0';
 
-    return token_length;
+    return quoted_flag;
 }
 
 /**
@@ -102,41 +191,12 @@ int getToken (const char *string, size_t *index, char *token, int token_max_leng
  * @return int 0 means no quotes, ~~1 means single quotes~~, 2 means double quotes
  */
 int getQuotedToken (const char *string, size_t *index, char *token, int token_max_length) {
-    skipWhitespace(string, index);
-
-    if (string[*index] == '\0') {
-        return -1;
+    const char *ptr = string + *index;
+    int result = getQuotedToken2(&ptr, token, token_max_length);
+    if (result >= 0) {
+        *index += result;
     }
-
-    int quoted_flag = (string[*index] == '"' ? 2 : 0);
-
-    int start_index = *index;
-
-    if (quoted_flag) {
-        start_index++;
-    }
-
-    // printf("Token starts at %d\n", start_index);
-
-    skipToken(string, index);
-
-    int token_length = *index - start_index;
-
-    if (quoted_flag) {
-        token_length--;
-    }
-
-    if (token_length > token_max_length) {
-        return -1;
-    }
-
-    // printf("Token is %d characters\n", token_length);
-
-    memcpy(token, string + start_index, token_length);
-
-    token[token_length] = '\0';
-
-    return quoted_flag;
+    return result;
 }
 
 int getNumericToken (const char *string, size_t *index) {
--- src/token.h
+++ src/token.h
@@ -1,5 +1,15 @@
 #include <stdlib.h>
 
+void skipWhitespace2 (const char **string);
+
+void skipToken2 (const char **string);
+
+void skipLine2 (const char **string);
+
+int getToken2 (const char **string, char *token, int token_max_length);
+
+int getQuotedToken2 (const char **string, char *token, int token_max_length);
+
 void skipWhitespace (const char *string, size_t *index);
 
 void skipToken (const char *string, size_t *index);
