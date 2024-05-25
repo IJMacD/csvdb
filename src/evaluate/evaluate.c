18ffacad275d2a297de4d2ef8e6e7439d6263793
Iain MacDonald <IJMacD@gmail.com>
2024-05-25 14:07:21 +0800

    Clear target for empty output

--- src/evaluate/evaluate.c
+++ src/evaluate/evaluate.c
@@ -11,14 +11,13 @@
 #include "../functions/date.h"
 #include "../functions/util.h"
 
-static int evaluateField (
+static int evaluateField(
     struct Table *tables,
     RowListIndex row_list,
     int index,
     struct Field *field,
-    char * output,
-    int max_length
-);
+    char *output,
+    int max_length);
 
 /**
  * @brief
@@ -31,15 +30,16 @@ static int evaluateField (
  * @param max_length
  * @return int Number of bytes written
  */
-int evaluateNode (
+int evaluateNode(
     struct Table *tables,
     RowListIndex row_list,
     int index,
     struct Node *node,
     char *output,
-    int max_length
-) {
-    if (node->function == FUNC_UNITY) {
+    int max_length)
+{
+    if (node->function == FUNC_UNITY)
+    {
         // With FUNC_UNITY we'll just output directly to parent
         return evaluateField(
             tables,
@@ -47,11 +47,11 @@ int evaluateNode (
             index,
             (struct Field *)node,
             output,
-            max_length
-        );
+            max_length);
     }
 
-    if ((node->function & MASK_FUNC_FAMILY) == FUNC_FAM_OPERATOR) {
+    if ((node->function & MASK_FUNC_FAMILY) == FUNC_FAM_OPERATOR)
+    {
         int result = evaluateOperatorNode(tables, row_list, index, node);
 
         sprintf(output, "%s", result ? "1" : "0");
@@ -59,7 +59,8 @@ int evaluateNode (
         return 1;
     }
 
-    if (node->child_count == -1) {
+    if (node->child_count == -1)
+    {
         // Optimistation where node is its own child
 
         char value[MAX_VALUE_LENGTH];
@@ -69,10 +70,9 @@ int evaluateNode (
             index,
             (struct Field *)node,
             value,
-            MAX_VALUE_LENGTH
-        );
+            MAX_VALUE_LENGTH);
 
-        char *values[] = { value };
+        char *values[] = {value};
 
         return evaluateFunction(output, node->function, (char **)values, 1);
     }
@@ -81,35 +81,37 @@ int evaluateNode (
     // Evaluate all children first
 
     int value_count = node->child_count;
-    if (value_count == 0) {
+    if (value_count == 0)
+    {
         value_count = 1;
     }
 
     char *values = malloc(value_count * MAX_VALUE_LENGTH);
     char **values_ptrs = malloc(value_count * sizeof(values));
-    for (int i = 0; i < value_count; i++) {
+    for (int i = 0; i < value_count; i++)
+    {
         values_ptrs[i] = values + MAX_FIELD_LENGTH * i;
     }
 
-    if (values == NULL) {
+    if (values == NULL)
+    {
         fprintf(
             stderr,
             __FILE__ ":%d Unable to allocate %d bytes for %d child node values\n", __LINE__,
             value_count * MAX_VALUE_LENGTH,
-            value_count
-        );
+            value_count);
         exit(-1);
     }
 
-    for (int i = 0; i < node->child_count; i++) {
+    for (int i = 0; i < node->child_count; i++)
+    {
         evaluateNode(
             tables,
             row_list,
             index,
             &node->children[i],
             values_ptrs[i],
-            MAX_VALUE_LENGTH
-        );
+            MAX_VALUE_LENGTH);
         // fprintf(stderr, "[EVALUATE] operand = '%s'\n", values_ptrs[i]);
     }
 
@@ -119,9 +121,13 @@ int evaluateNode (
         output,
         node->function,
         values_ptrs,
-        node->child_count
-    );
+        node->child_count);
     // fprintf(stderr, "[EVALUATE] return value = '%s'\n", output);
+
+    if (result == 0)
+    {
+        output[0] = '\0';
+    }
 
     free(values);
     free(values_ptrs);
@@ -130,60 +136,67 @@ int evaluateNode (
 }
 
 // Evaluates functions on nodes with purely constant children
-int evaluateConstantNode (
+int evaluateConstantNode(
     struct Node *node,
-    char *output
-) {
-    if (node->function == FUNC_UNITY) {
+    char *output)
+{
+    if (node->function == FUNC_UNITY)
+    {
         // If this node is just unity, then just copy from the field
         strcpy(output, node->field.text);
         return 1;
     }
 
     int value_count = node->child_count;
-    if (value_count <= 0) {
+    if (value_count <= 0)
+    {
         value_count = 1;
     }
 
     char *values = malloc(value_count * MAX_VALUE_LENGTH);
     char **values_ptrs = malloc(value_count * sizeof(values));
-    for (int i = 0; i < value_count; i++) {
+    for (int i = 0; i < value_count; i++)
+    {
         values_ptrs[i] = values + MAX_FIELD_LENGTH * i;
     }
 
-    if (values == NULL) {
+    if (values == NULL)
+    {
         fprintf(
             stderr,
             __FILE__ ":%d Unable to allocate %d bytes for %d child node values\n", __LINE__,
             value_count * MAX_VALUE_LENGTH,
-            value_count
-        );
+            value_count);
         exit(-1);
     }
 
-    if (node->child_count == -1) {
+    if (node->child_count == -1)
+    {
         // If this node is a self-child node, just copy from the field (since we
         // know the field must be constant)
         strcpy(values_ptrs[0], node->field.text);
     }
-    else for (int i = 0; i < node->child_count; i++) {
-        struct Node *child_node = &node->children[i];
+    else
+        for (int i = 0; i < node->child_count; i++)
+        {
+            struct Node *child_node = &node->children[i];
 
-        if (child_node->function == FUNC_UNITY) {
-            evaluateConstantField(values_ptrs[i], &child_node->field);
+            if (child_node->function == FUNC_UNITY)
+            {
+                evaluateConstantField(values_ptrs[i], &child_node->field);
+            }
+            else
+            {
+                evaluateConstantNode(child_node, values_ptrs[i]);
+            }
+            // fprintf(stderr, "[EVALUATE] operand = '%s'\n", values_ptrs[i]);
         }
-        else {
-            evaluateConstantNode(child_node, values_ptrs[i]);
-        }
-        // fprintf(stderr, "[EVALUATE] operand = '%s'\n", values_ptrs[i]);
-    }
 
     int result = evaluateFunction(
         output,
         node->function,
         values_ptrs,
-        node->child_count
-    );
+        node->child_count);
     // fprintf(stderr, "[EVALUATE] return value = '%s'\n", output);
 
     free(values);
@@ -205,35 +218,37 @@ int evaluateConstantNode (
  * @param max_length
  * @return int Number of bytes written
  */
-int evaluateNodeList (
-    struct Table * tables,
+int evaluateNodeList(
+    struct Table *tables,
     RowListIndex row_list,
     int index,
     struct Node *nodes,
     int node_count,
     char *output,
-    __attribute__ ((unused)) int max_length
-) {
+    __attribute__((unused)) int max_length)
+{
     char value[MAX_VALUE_LENGTH];
     int bytes_written = 0;
 
-    for (int j = 0; j < node_count; j++) {
+    for (int j = 0; j < node_count; j++)
+    {
         int count = evaluateNode(
             tables,
             row_list,
             index,
             &nodes[j],
             value,
-            sizeof(value)
-        );
+            sizeof(value));
 
         // Numeric values need to be fixed width for comparison.
         // After testing it make no difference whether numeric values are
         // compared or strings are compared. (There are other slower steps).
-        if (is_numeric(value)) {
+        if (is_numeric(value))
+        {
             count = sprintf(output, "%020ld", atol(value));
         }
-        else {
+        else
+        {
             count = strlen(strcpy(output, value));
         }
 
@@ -258,47 +273,52 @@ int evaluateNodeList (
  * @param result_index
  * @return int number of chars written
  */
-static int evaluateField (
+static int evaluateField(
     struct Table *tables,
     RowListIndex row_list,
     int index,
     struct Field *field,
-    char * output,
-    int max_length
-) {
-    if (field->index == FIELD_UNKNOWN) {
+    char *output,
+    int max_length)
+{
+    if (field->index == FIELD_UNKNOWN)
+    {
         fprintf(stderr, "Trying to evaluate unknown field\n");
         exit(-1);
     }
 
-    if (field->index == FIELD_CONSTANT) {
+    if (field->index == FIELD_CONSTANT)
+    {
         return evaluateConstantField(output, field);
     }
 
-    if (field->table_id >= 0) {
+    if (field->table_id >= 0)
+    {
         int row_id;
 
-        if (row_list == ROWLIST_ROWID) {
+        if (row_list == ROWLIST_ROWID)
+        {
             // Special value of row_list means: row_index === row_id
             row_id = index;
         }
-        else {
+        else
+        {
             row_id = getRowID(getRowList(row_list), field->table_id, index);
         }
 
-        if (field->index == FIELD_ROW_INDEX) {
+        if (field->index == FIELD_ROW_INDEX)
+        {
             return sprintf(output, "%d", row_id);
         }
 
         struct DB *db = tables[field->table_id].db;
 
         return getRecordValue(
-            db,
-            row_id,
-            field->index,
-            output,
-            max_length
-        ) > 0;
+                   db,
+                   row_id,
+                   field->index,
+                   output,
+                   max_length) > 0;
     }
 
     output[0] = '\0';
@@ -313,39 +333,42 @@ static int evaluateField (
  * @param field
  * @return int Number of chars written
  */
-int evaluateConstantField (char * output, struct Field *field) {
+int evaluateConstantField(char *output, struct Field *field)
+{
 
-    if (field->index != FIELD_CONSTANT) {
+    if (field->index != FIELD_CONSTANT)
+    {
         fprintf(
             stderr,
             "Tried to evaluate non-contant value as constant: %s\n",
-            field->text
-        );
+            field->text);
         exit(-1);
     }
 
     if (
-        field->text[0] == '0'
-        && field->text[1] == 'x'
-    ) {
+        field->text[0] == '0' && field->text[1] == 'x')
+    {
         long val = strtol(field->text, NULL, 16);
         return sprintf(output, "%ld", val);
     }
 
-    if (strcmp(field->text, "CURRENT_DATE") == 0) {
+    if (strcmp(field->text, "CURRENT_DATE") == 0)
+    {
         struct DateTime dt;
         parseDateTime("CURRENT_DATE", &dt);
         return sprintDate(output, &dt);
     }
 
-    if (strcmp(field->text, "CURRENT_TIME") == 0) {
+    if (strcmp(field->text, "CURRENT_TIME") == 0)
+    {
         struct DateTime dt;
         parseDateTime("CURRENT_TIME", &dt);
         return sprintf(output, "%02d:%02d:%02d", dt.hour, dt.minute, dt.second);
     }
 
     // Alpine doesn't like sprintf'ing a buffer into itself
-    if (output == field->text) {
+    if (output == field->text)
+    {
         // fprintf(stderr, "[DEBUG] src = dest\n");
         return strlen(output);
     }
@@ -355,17 +378,20 @@ int evaluateConstantField (char * output, struct Field *field) {
 
 /**
  * returns 1 if node is entirely constant; 0 if any part of it includes fields.
-*/
-int isConstantNode (struct Node *node) {
+ */
+int isConstantNode(struct Node *node)
+{
     if (node->function == FUNC_UNITY
         // Optimistation where node is its own child
-        || node->child_count == -1
-    ) {
+        || node->child_count == -1)
+    {
         return node->field.index == FIELD_CONSTANT;
     }
 
-    for (int i = 0; i < node->child_count; i++) {
-        if (isConstantNode(&node->children[i]) == 0) {
+    for (int i = 0; i < node->child_count; i++)
+    {
+        if (isConstantNode(&node->children[i]) == 0)
+        {
             return 0;
         }
     }
@@ -386,19 +412,19 @@ void evaluateNodeTreePartial(
     int list_id,
     int result_index,
     struct Node *node,
-    int max_table_id
-) {
+    int max_table_id)
+{
     int bit_map_limit = 1 << (max_table_id + 1);
 
-    if (getTableBitMap(node) < bit_map_limit) {
+    if (getTableBitMap(node) < bit_map_limit)
+    {
         evaluateNode(
             tables,
             list_id,
             result_index,
             node,
             node->field.text,
-            MAX_FIELD_LENGTH
-        );
+            MAX_FIELD_LENGTH);
 
         node->function = FUNC_UNITY;
         node->field.index = FIELD_CONSTANT;
@@ -406,7 +432,8 @@ void evaluateNodeTreePartial(
         return;
     }
 
-    for (int i = 0; i < node->child_count; i++) {
+    for (int i = 0; i < node->child_count; i++)
+    {
         struct Node *child = &node->children[i];
 
         evaluateNodeTreePartial(
@@ -414,7 +441,6 @@ void evaluateNodeTreePartial(
             list_id,
             result_index,
             child,
-            max_table_id
-        );
+            max_table_id);
     }
 }
